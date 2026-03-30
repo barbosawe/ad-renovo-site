2394657eeda62c1d740e571711de3e116f2f0954
Wellington <wellbarbosaft@gmail.com>
2026-03-30 14:36:55 -0300

    Configurando STATIC_ROOT para o Render

--- setup/settings.py
+++ setup/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# ADICIONE ESTA LINHA AQUI PARA ELE ACHAR SUA IMAGEM:
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
