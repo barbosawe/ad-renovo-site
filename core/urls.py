6d75000d463312c796755ba3b56286d4425792d4
Wellington <wellbarbosaft@gmail.com>
2026-04-03 16:08:34 -0300

    Versão 1.8 TESTE

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='inicio'),
     path('enviar-pedido/', views.enviar_pedido, name='enviar_pedido'),
+    path('galeria-kids/', views.galeria_kids, name='galeria_kids'),
+    path('galeria-jovens/', views.galeria_jovens, name='galeria_jovens'),
+    path('galeria-circulo/', views.galeria_circulo, name='galeria_circulo'),
+    path('galeria-music/', views.galeria_music, name='galeria_music'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.shortcuts import render
 
 # 1. Função que carrega a página inicial
 def index(request):
@@ -35,3 +36,20 @@ def enviar_pedido(request):
         return redirect('inicio')
 
     return render(request, 'index.html')
+
+# 3. Função para a Galeria Kids, Jovens, Círculo de Oração e Ministério de Música
+
+
+# Sua view da página inicial (index) deve estar aqui em cima...
+
+def galeria_kids(request):
+    return render(request, 'galeria_kids.html')
+
+def galeria_jovens(request):
+    return render(request, 'galeria_jovens.html')
+
+def galeria_circulo(request):
+    return render(request, 'galeria_circulo.html')
+
+def galeria_music(request):
+    return render(request, 'galeria_music.html')
